c3fb35fc7975bc51aaf8ca95784809d4c7be99d7
Indrajeet1256 <Indrajeetgiri007@gmail.com>
2024-12-01 14:04:32 +0530

    filters & view updated

--- src/d.types.ts
+++
@@ -1,9 +0,0 @@
-import { RowData } from "@tanstack/react-table";
-
-declare module "@tanstack/table-core" {
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	interface TableMeta<TData extends RowData> {
-		filter: string;
-		viewType: string;
-	}
-}
